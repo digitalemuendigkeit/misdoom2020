1a2f54ef382b22ff3bf24bfde257f3d0d824d9f1
André Calero Valdez <andrecalerovaldez@gmail.com>
2020-01-15 10:02:01 +0100

    updated installer

--- Misdoom/install_required_pkg.r
+++ Misdoom/install_required_pkg.r
@@ -5,3 +5,9 @@ install.packages("tidyverse")
 install.packages("DT")
 install.packages("rmdformats")
 install.packages("prettydoc")
+
+
+if(!require(remotes)){
+  install.packages("remotes")
+}
+remotes::install_github("statisticsforsocialscience/rmd_templates")
